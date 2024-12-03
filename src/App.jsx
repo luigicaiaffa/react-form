05daeb4a8daf26189d5a9f068ebb14f6ee506477
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-12-03 14:59:00 +0100

    add page main structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,29 @@ function App() {
   return (
     <>
       <header>
-        <div className="container">
-          <h1 className="p-4 fw-bold">React Form</h1>
-        </div>
+        <h1 className="p-4 fw-bold">React Form</h1>
       </header>
 
       <main>
+        <div className="container">
+          <form className="p-4">
+            <div class="mb-3">
+              <label for="newArticle" className="form-label">
+                Inserisci nuovo articolo
+              </label>
+              <input type="text" className="form-control" id="newArticle" />
+            </div>
+            <button type="submit" className="btn btn-success">
+              <i className="fa-solid fa-plus fa-xl"></i>
+            </button>
+          </form>
 
+          <div className="p-4">
+            <ul className="list-group">
+              <li className="list-group-item">Nuovo Articolo</li>
+            </ul>
+          </div>
+        </div>
       </main>
     </>
   );
